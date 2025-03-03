767d5a12eb1bd59c0af0366bbfd0aa6ff1a9a7b8
jeri <jerimonski@gmail.com>
2025-03-03 19:33:08 -0300

    feat: New CarShop Design

--- src/pages/CarShop.jsx
+++ src/pages/CarShop.jsx
@@ -5,36 +5,71 @@ function CarShop() {
 
   return (
     <div className="max-w-[1600px] mx-auto py-12">
-      <section className="flex justify-center bg-blue-200 ">
+      <section>
         {cart.length === 0 ? (
           <article className="text-center">
             <h1 className="text-3xl font-bold">Shopping Cart</h1>
             <p>Your cart is empty 😢</p>
           </article>
         ) : (
-          <ul>
-            {cart.map((item) => (
-              <li key={item.id} className="border-b py-2">
-                {item.name} - ${item.price.toFixed(2)}
-                <img src={item.image} alt={item.name} className="h-32" />
-                <p>{item.size}</p>
-                <button
-                  onClick={() =>
-                    dispatch({ type: "REMOVE_FROM_CART", payload: item.id })
-                  }
-                  className="ml-4 text-red-500"
-                >
-                  Remove
-                </button>
-              </li>
-            ))}
+          <>
             <button
               onClick={() => dispatch({ type: "CLEAR_CART" })}
-              className="mt-4 bg-red-500 text-white px-4 py-2"
+              className="my-2 px-2 py-1 rounded bg-red-500 hover:bg-red-600 text-white duration-200"
             >
               Clear Cart
             </button>
-          </ul>
+
+            <article className="grid grid-cols-2">
+              <div className="h-full space-y-2">
+                {cart.map((item) => (
+                  <div
+                    key={item.id}
+                    className="border border-2 border-black rounded flex px-2 w-[350px] items-center"
+                  >
+                    <img src={item.image} alt={item.name} className="h-28" />
+                    <div>
+                      <p className="font-semibold text-md">{item.name}</p>
+                      <p>
+                        <span className="font-semibold">size:</span> {item.size}
+                      </p>
+                      <div className="relative">
+                        <span className="font-semibold">price:</span> $
+                        {item.price.toFixed(2)}
+                        <button
+                          onClick={() =>
+                            dispatch({
+                              type: "REMOVE_FROM_CART",
+                              payload: item.id,
+                            })
+                          }
+                          className="absolute right-0 text-red-500 hover:text-red-800 duration-200"
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="bg-smoke-gray h-max mx-4 px-4">
+                <h3 className="text-center text-2xl font-semibold">
+                  Order summary
+                </h3>
+                <p className="font-semibold text-xl flex justify-between py-4">
+                  Total
+                  <span>
+                    $
+                    {cart.reduce((acc, item) => acc + item.price, 0).toFixed(2)}
+                  </span>
+                </p>
+
+                <button className="bg-black text-white rounded px-4 py-2 w-full hover:scale-105 duration-200">
+                  Process To Checkout
+                </button>
+              </div>
+            </article>
+          </>
         )}
       </section>
     </div>
